73ef39cc745b1a99feec1156ff8d33ea6a828555
Tulasi <tulasich72@gmail.com>
2022-07-01 09:59:03 +0530

    Letter to yourself

---
+++ Programming-by-doing/BasicsAndPrinting/Letterbutsimple.java
@@ -0,0 +1,17 @@
+//Rather simple letter - pretty basic
+//Programming by doing exercise - 1
+public class Letterbutsimple {
+    public static void main(String[] args) {
+        System.out.println("+---------------------------------------------------------+");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                         |");
+        System.out.println("|                                                         |");
+        System.out.println("|                              Bill Gates                 |");
+        System.out.println("|                              1 Microsoft Way            |");
+        System.out.println("|                              Redmond, WA 98104          |");
+        System.out.println("|                                                         |");
+        System.out.println("+---------------------------------------------------------+");
+    }
+}
