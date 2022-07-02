a5444ed589a955dc41ee468924cb0f3126453a7b
Tulasi <tulasich72@gmail.com>
2022-07-02 17:50:24 +0530

    Basic Printing programs

---
+++ Programming-by-doing/BasicsAndPrinting/NumbersAndMath.java
@@ -0,0 +1,29 @@
+public class NumbersAndMath {
+    public static void main(String[] args) {
+
+		System.out.println( "I will now count my marks:" );
+
+		System.out.println( "English " + ( (15.5 + 20) / 2 ) );
+		System.out.println( "Physics " + ( (17.5 + 19.5)/2 ) );
+
+		System.out.println( "Now I will count the problems:" );
+
+		System.out.println( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 );
+
+		System.out.println( "Is it true that 3.2 + 2 < 5.1 - 7.0?" );
+
+		System.out.println( 3.2 + 2 < 5.1 - 7.0 );
+
+		System.out.println( "What is 3.2 + 2? " + ( 3.2 + 2 ) );
+		System.out.println( "What is 5.1 - 7.0? " + ( 5 - 7 ) );
+
+		System.out.println( "Oh, that's why it's false." );
+
+		System.out.println( "How about some more." );
+
+		System.out.println( "Is it greater? " + ( 5.25 > -2 ) );
+		System.out.println( "Is it greater or equal? " + ( 5 >= -2 ) );
+		System.out.println( "Is it less or equal? " + ( 5 <= -2 ) );
+
+    }
+}
---
+++ Programming-by-doing/BasicsAndPrinting/PrintInitials.java
@@ -0,0 +1,10 @@
+ public class PrintInitials {
+    public static void main(String[] args) {
+        System.out.println("My name is Tulasi Ch");
+        System.out.println("     T T T T T T        C C C        H     H     ");
+        System.out.println("          T            C     C       H     H     ");
+        System.out.println("          T            C             H H H H     ");
+        System.out.println("          T            C     C       H     H     ");
+        System.out.println("          T             C C C        H     H     ");
+    }
+}
